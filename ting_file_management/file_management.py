f02302d89b52da7c71cba3a31b2eb919189a82a0
biel-igl <gabrielpires524@gmail.com>
2023-07-17 17:58:23 -0300

    feat: faz req 02

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        sys.stderr.write("Formato inválido")
+    try:
+        with open(path_file, "r") as text:
+            lines = text.readlines()
+            for i in range(len(lines)):
+                lines[i] = lines[i].rstrip()
+            return lines
+
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
