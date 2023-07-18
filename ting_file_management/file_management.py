ca3c9c7361bb690fa9222df9959370da16f56cf2
biel-igl <gabrielpires524@gmail.com>
2023-07-18 15:49:41 -0300

    feat: faz req 03

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -6,10 +6,8 @@ def txt_importer(path_file):
         sys.stderr.write("Formato inválido")
     try:
         with open(path_file, "r") as text:
-            lines = text.readlines()
-            for i in range(len(lines)):
-                lines[i] = lines[i].rstrip()
-            return lines
+            lines = [line.rstrip() for line in text]
+        return lines
 
     except FileNotFoundError:
         print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for item in instance._data:
+        if item["nome_do_arquivo"] == path_file:
+            break
+    else:
+        lines = txt_importer(path_file)
+        data = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(lines),
+            "linhas_do_arquivo": lines,
+        }
+        instance.enqueue(data)
+        print(data, file=sys.stdout)
 
 
 def remove(instance):
