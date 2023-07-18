bf5676c1c5377b9bac98ffc716c8a77b7e5a6999
biel-igl <gabrielpires524@gmail.com>
2023-07-18 19:14:11 -0300

    feat: faz req -7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,18 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    ocorrencias = []
+    for item in instance._data:
+        arquivo = item["nome_do_arquivo"]
+        lines_find = []
+        for index, line in enumerate(item["linhas_do_arquivo"], start=1):
+            if word.lower() in line.lower():
+                lines_find.append({"linha": index})
+        if lines_find:
+            ocorrencias.append({
+                "palavra": word,
+                "arquivo": arquivo,
+                "ocorrencias": lines_find
+            })
+    return ocorrencias
 
 
 def search_by_word(word, instance):
