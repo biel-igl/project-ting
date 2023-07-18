0287558881883d6ac789fc82e5bebf5eebaf6add
biel-igl <gabrielpires524@gmail.com>
2023-07-18 16:51:27 -0300

    feat: faz tests req 06

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,23 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    queue = PriorityQueue()
+
+    ex1 = {"nome": "aqui.txt", "qtd_linhas": 5}
+    ex2 = {"nome": "aqui.txt", "qtd_linhas": 1}
+
+    queue.enqueue(ex1)
+    assert len(queue) == 1
+
+    queue.dequeue()
+    assert len(queue) == 0
+
+    queue.enqueue(ex1)
+    queue.enqueue(ex2)
+    assert queue.search(0) == ex2
+    assert queue.search(1) == ex1
+
+    with pytest.raises(IndexError, match="Índice Inválido ou Inexistente"):
+        queue.search(17)
